e4e047f0498c17fed5bc84e99f4f6770444ab11b
dsidler88 <dsidler88@gmail.com>
2022-06-07 13:52:27 -0400

    working version of chromedriver-linux

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -7,7 +7,7 @@
 #     <testng-results ignored="0" total="23" passed="22" failed="1" skipped="0">                  #
 ###################################################################################################
 
-TEST_RESULTS_LOCATION="${1:-/home/runner/work/selenium-webdriver-java-course/selenium-webdriver-java-course/webdriver_java/target/surefire-reports}"
+TEST_RESULTS_LOCATION="${1:-/home/runner/work/Sample_Actions/Sample_Actions/webdriver_java/target/surefire-reports}"
 TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "<testng-results")
 echo "IGNORED_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')"
 echo "TOTAL_TESTS=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')"
